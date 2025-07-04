f8a9377f0cfb02ebafa6dcdd8c93cf10f65a9691
Aditya NV <138525577+aditya-nv-06@users.noreply.github.com>
2025-07-04 14:03:20 +0530

    Update HP.java

--- HP.java
+++ HP.java
@@ -1,5 +1,5 @@
 public class HP{
-public static void main(String[] args)
+public static void main(String[] args){
 	System.out.println("Hi");
 	System.out.println("Hello");
         System.out.println("Webhook done");
