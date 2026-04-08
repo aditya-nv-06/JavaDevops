5fcf2cc1ed2c2e249181e0100e891d35cb6a8a4d
Aditya NV <138525577+aditya-nv-06@users.noreply.github.com>
2026-04-08 14:18:55 +0530

    Update Gitproject.java

--- src/test/java/gitpackage/Gitproject.java
+++ src/test/java/gitpackage/Gitproject.java
@@ -4,7 +4,7 @@ public class Gitproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi welcome.");
+		System.out.println("hi welcome. to  devops maven");
 	}
 
 }
